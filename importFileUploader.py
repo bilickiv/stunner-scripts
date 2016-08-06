03961f065b469b3041fa35bba50233071fced2a1
bilickiv <bilickiv@gmail.com>
2016-08-06 12:19:40 +0200

    database string changed

--- importFileUploader.py
+++ importFileUploader.py
@@ -17,7 +17,7 @@ config = {
 connection = pymysql.connect(host='10.6.14.37',
                              user='root',
                              password='',
-                             db='test',
+                             db='stunner',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.Cursor)
 def uploadWithOtherDriver(name):
