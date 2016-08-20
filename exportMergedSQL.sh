d065220b52af9c39a59b7c92a84e5bfd8b74f6f3
Bilicki Vilmos <bilickiv@Bilickis-iMac.local>
2016-08-20 17:27:28 +0200

    first sql exporter test

--- exportMergedSQL.sh
+++ exportMergedSQL.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-mysql -h 127.0.0.1 -u root -e "use test; select * from DATA where pubIP is not null limit 10" > test.txt
+mysql -h 127.0.0.1 -u root -e sqlIP2LocationAndMainDataExporterScript.sql > test.txt
---
+++ sqlIP2LocationAndMainDataExporterScript.sql
@@ -0,0 +1,2 @@
+use test;
+select * from DATA where pubIP is not null limit 10
