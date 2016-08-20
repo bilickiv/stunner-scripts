0ca4a2a1eec1c913211f0f18d572dda7e951db78
Bilicki Vilmos <bilickiv@Bilickis-iMac.local>
2016-08-20 17:20:59 +0200

    SQL magic....

--- sqlHistory.sql
+++ sqlHistory.sql
@@ -6,10 +6,19 @@ delete from `dbip_lookup`
 drop table `dbip_lookup`
 
 CREATE TABLE `dbip_lookup` (
-  `ip_start` varbinary(16) NOT NULL,
-  `ip_end` varbinary(16) NOT NULL,
+  `ip_start`  text CHARACTER SET utf8 COLLATE utf8_general_ci,
+  `ip_end`  text CHARACTER SET utf8 COLLATE utf8_general_ci,
   `country` char(2) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `stateprov` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   `city` varchar(80) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
   PRIMARY KEY (`ip_start`)
 );
+
+alter table `dbip_lookup` add ipEnd INTEGER
+alter table DATA add pubIP INTEGER
+
+update `dbip_lookup` set `ipStart` = INET_ATON(REPLACE(ip_start,'"','')), `ipEnd` = INET_ATON(REPLACE(ip_end,'"',''))
+update `DATA` set `pubIP` = INET_ATON(REPLACE(publicIP,'"',''))
+
+
+select * from (select * from DATA where pubIP is not null limit 10) d JOIN dbip_lookup l on d.pubIP BETWEEN l.ipStart AND l.ipEnd 
