880ee288f9e5e6237a643101d8c635f2b600b842
Bilicki Vilmos <bilickiv@Bilickis-iMac.local>
2016-08-21 08:40:11 +0200

    old sql

--- sqlIP2LocationAndMainDataExporterScript.sql
+++ sqlIP2LocationAndMainDataExporterScript.sql
@@ -1,3 +1,3 @@
 use test;
-#select d.*, l.`country`, l.`stateprov`, l.`city` from  DATA  d  LEFT JOIN dbip_lookup l on d.pubIP BETWEEN l.ipStart AND l.ipEnd
-select d.* from  DATA d
+select d.*, l.`country`, l.`stateprov`, l.`city` from  DATA  d  LEFT JOIN dbip_lookup l on d.pubIP BETWEEN l.ipStart AND l.ipEnd
+#select d.* from  DATA d
