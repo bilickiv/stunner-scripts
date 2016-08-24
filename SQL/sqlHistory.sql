9f9e886c6856061e93f5c1677c0e4d16d786b1a4
bilickiv <bilickiv@gmail.com>
2016-08-24 17:05:06 +0200

    new sql filter expressions

--- SQL/sqlHistory.sql
+++ SQL/sqlHistory.sql
@@ -24,4 +24,9 @@ update `DATA` set `pubIP` = INET_ATON(REPLACE(publicIP,'"',''))
 select * from (select * from DATA where pubIP is not null limit 10) d JOIN dbip_lookup l on d.pubIP BETWEEN l.ipStart AND l.ipEnd 
 SELECT `hashId`, CONCAT(EXTRACT(YEAR_MONTH from CAST(mdate as DATETIME)),EXTRACT(DAY from CAST(mdate as DATETIME))), `publicIP`, localIP,  count(*) from DATA group by hashId, CONCAT(EXTRACT(YEAR_MONTH from CAST(mdate as DATETIME)),EXTRACT(DAY from CAST(mdate as DATETIME))), publicIP order by hashId, CONCAT(EXTRACT(YEAR_MONTH from CAST(mdate as DATETIME)),EXTRACT(DAY from CAST(mdate as DATETIME))), publicIP
 
-SELECT DISTINCT localIP from DATA where  IS_IPV4(localIP) and (left(localIP,3) not like "10.") and (left(localIP,7) not like '192.168') and localIP not in (SELECT DISTINCT ipAddress from IP2ISPDATA) order by localIP
+SELECT DISTINCT localIP from DATA where left(localIP,1)  REGEXP '^-?[0-9]+$' AND localIP not IN (SELECT DISTINCT ipAddress from IP2ISPDATA) and (left(localIP,3) not like "10.") and (left(localIP,7) not like '192.168')  AND NOT (left(localIP,3) LIKE '172' and SUBSTRING(localIP,5,2) REGEXP '^-?[0-9]+$' AND (SUBSTRING(localIP,5,2) > 15 ) AND (SUBSTRING(localIP,5,2) < 32 )) order by localIP
+
+
+INSERT INTO TMPIP2DATA (publicIP) SELECT DISTINCT localIP from DATA where left(localIP,1)  REGEXP '^-?[0-9]+$' AND localIP not IN (SELECT DISTINCT publicIP from TMPIP2DATA) and (left(localIP,3) not like "10.") and (left(localIP,7) not like '192.168')  AND NOT (left(localIP,3) LIKE '172' and SUBSTRING(localIP,5,2) REGEXP '^-?[0-9]+$' AND (SUBSTRING(localIP,5,2) > 15 ) AND (SUBSTRING(localIP,5,2) < 32 )) order by localIP
+
+SELECT distinct localIP, connectionMode, hashId from DATA WHERE localIP not like '0.0.0.0' and (left(localIP,3) not like "10.") and (left(localIP,7) not like '192.168')  AND NOT (left(localIP,3) LIKE '172' and SUBSTRING(localIP,5,2) REGEXP '^-?[0-9]+$' AND (SUBSTRING(localIP,5,2) > 15 ) AND (SUBSTRING(localIP,5,2) < 32 )) order by localIP
