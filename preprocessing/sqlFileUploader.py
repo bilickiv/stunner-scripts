10b48e5e968c21c4f35b0c707852808d4f4ef335
bilickiv <bilickiv@gmail.com>
2016-12-03 09:53:06 +0100

    filtered result

--- preprocessing/sqlFileUploader.py
+++ preprocessing/sqlFileUploader.py
@@ -13,6 +13,13 @@ connection = pymysql.connect(host='10.6.14.37',
                              charset='utf8mb4',
                              local_infile=True,
                              cursorclass=pymysql.cursors.Cursor)
+connectionFiltered = pymysql.connect(host='10.6.14.36',
+                             user='root',
+                             password='',
+                             db='stunner',
+                             charset='utf8mb4',
+                             local_infile=True,
+                             cursorclass=pymysql.cursors.Cursor)                             
 def uploadFile(name):
     try:
         with connection.cursor() as cursor:
@@ -26,14 +33,28 @@ def uploadFile(name):
     finally:
         print('Finished')    
     return
+def uploadFilteredFile(name):
+    try:
+        with connectionFiltered.cursor() as cursor:
+            # Create a new record
+            sql = "LOAD DATA LOCAL INFILE '" + name + \
+            "'  IGNORE INTO TABLE DATAPART FIELDS TERMINATED BY ';'"
+            cursor.execute(sql)
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        connectionFiltered.commit()
+    finally:
+        print('Finished')    
+    return    
 import glob
-path = "/home/bilickiv/data/raw_dataset/userSpecificPreprocessed/*.csv"
+path = "/home/bilickiv/data/raw_dataset/userSpecificPreprocessedSubset/*.csv"
+
 for fname in glob.glob(path):
     print("Loading file:" + fname + "----" + unicode(datetime.datetime.now()))
 #    uploadFile(fname)
     print("Finished loading file:" + fname + "----" + unicode(datetime.datetime.now()))
-    uploadFile(fname)
-          
+    #uploadFile(fname)
+    uploadFilteredFile(name)          
 
               
 #loadFile("../402036")      
