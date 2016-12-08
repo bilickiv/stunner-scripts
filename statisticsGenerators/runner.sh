dde854b0a6649298e6e7a65dc8bcc669ffa0b612
bilickiv <bilickiv@gmail.com>
2016-12-08 13:10:14 +0100

    config change

--- statisticsGenerators/runner.sh
+++ statisticsGenerators/runner.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-nohup python3 simpleFirstOrderStat.py linux 0 > log0.txt &
-nohup python3 simpleFirstOrderStat.py linux 1 > log1.txt &
-nohup python3 simpleFirstOrderStat.py linux 2 > log2.txt &
-nohup python3 simpleFirstOrderStat.py linux 3 > log3.txt &
-nohup python3 simpleFirstOrderStat.py linux 4 > log4.txt &
-nohup python3 simpleFirstOrderStat.py linux 5 > log5.txt &
-nohup python3 simpleFirstOrderStat.py linux 6 > log6.txt &
-nohup python3 simpleFirstOrderStat.py linux 7 > log7.txt &
-nohup python3 simpleFirstOrderStat.py linux 8 > log8.txt &
-nohup python3 simpleFirstOrderStat.py linux 9 > log9.txt &
-nohup python3 simpleFirstOrderStat.py linux 10 > log10.txt &
-nohup python3 simpleFirstOrderStat.py linux 11 > log11.txt &
-nohup python3 simpleFirstOrderStat.py linux 12 > log12.txt &
+nohup python3 simpleFirstOrderStat.py 1 0 > log0.txt &
+nohup python3 simpleFirstOrderStat.py 1 1 > log1.txt &
+nohup python3 simpleFirstOrderStat.py 1 2 > log2.txt &
+nohup python3 simpleFirstOrderStat.py 1 3 > log3.txt &
+nohup python3 simpleFirstOrderStat.py 1 4 > log4.txt &
+nohup python3 simpleFirstOrderStat.py 1 5 > log5.txt &
+nohup python3 simpleFirstOrderStat.py 1 6 > log6.txt &
+nohup python3 simpleFirstOrderStat.py 1 7 > log7.txt &
+nohup python3 simpleFirstOrderStat.py 1 8 > log8.txt &
+nohup python3 simpleFirstOrderStat.py 1 9 > log9.txt &
+nohup python3 simpleFirstOrderStat.py 1 10 > log10.txt &
+nohup python3 simpleFirstOrderStat.py 1 11 > log11.txt &
+nohup python3 simpleFirstOrderStat.py 1 12 > log12.txt &
