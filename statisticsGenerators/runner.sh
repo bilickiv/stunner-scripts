cb39aa247905b47b1c56fbc0e57d94f6c61eb96f
bilickiv <bilickiv@gmail.com>
2016-12-09 14:37:04 +0100

    addied new processes to script

--- statisticsGenerators/runner.sh
+++ statisticsGenerators/runner.sh
@@ -12,3 +12,13 @@ nohup python3 simpleFirstOrderStat.py 1 9 > log9.txt &
 nohup python3 simpleFirstOrderStat.py 1 10 > log10.txt &
 nohup python3 simpleFirstOrderStat.py 1 11 > log11.txt &
 nohup python3 simpleFirstOrderStat.py 1 12 > log12.txt &
+nohup python3 simpleFirstOrderStat.py 1 13 > log13.txt &
+nohup python3 simpleFirstOrderStat.py 1 14 > log14.txt &
+nohup python3 simpleFirstOrderStat.py 1 15 > log15.txt &
+nohup python3 simpleFirstOrderStat.py 1 16 > log16.txt &
+nohup python3 simpleFirstOrderStat.py 1 17 > log17.txt &
+nohup python3 simpleFirstOrderStat.py 1 18 > log18.txt &
+nohup python3 simpleFirstOrderStat.py 1 19 > log19.txt &
+nohup python3 simpleFirstOrderStat.py 1 20 > log20.txt &
+
+
