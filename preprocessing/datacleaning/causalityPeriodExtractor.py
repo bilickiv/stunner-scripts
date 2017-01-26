c2f777766e22d7cbbde276fb69a59378c3920a6e
bilickiv <bilickiv@gmail.com>
2017-01-26 17:12:27 +0100

    The central error collector is still empty

--- preprocessing/datacleaning/causalityPeriodExtractor.py
+++ preprocessing/datacleaning/causalityPeriodExtractor.py
@@ -13,37 +13,83 @@ hashIds = set()
 userSpecificPreprocessedFolder = ""
 userSpecificPreprocessedCausalityReports = ""
 actualEnvironment = "osx"
+errorLogCollector = pd.DataFrame(columns=('Type','StartDate', 'EndDate', 'Count', 'Error', 'HashId'))
 def createTimeAnalysis(data):
-    uploadDate = "1970-01-01"
-    mdate = "1970-01-01"
-    data.sort_values(by=[0,1], ascending=[True,True])
-    print(data.head(10))
-    count = 0
+    endUDate = "1970-01-01"
+    endADate = "1970-01-01"
+    errorLog = {}
+    fileNumber = data[0].str.replace('/home/bilickiv/data/raw_dataset/unzipped_dataset/','')
+    fileNumber = fileNumber.str.replace('.csv','')
+    fileNumber = fileNumber.str.replace('SQL:/home/bilickiv//data/raw_dataset/old_data/datacollector-','')
+    fileNumber = fileNumber.str.replace('.sql.blob','')
+    fileNumber = fileNumber.str.replace('2015-01-13','13')
+    fileNumber = pd.to_numeric(fileNumber, errors='coerce')
+    #fileNumber = fileNumber.to_numeric()            
+    data['globalIndex'] = fileNumber
+    df = data[['globalIndex',1,2,7,3]]
+    df = df.sort_values(by=['globalIndex',1], ascending=[True,True])
+    #print(df.head(1000))
+    #print(data.head(10))
+    countA = 0
+    countU = 0    
     tmpdate = ""
-    for index, row in data.iterrows():
-        count=+1
+    firstUDate = ""
+    firstADate = ""    
+    for index, row in df.iterrows():
+        hashId = row[3]
+        firstUDate = ""
+        firstADate = ""
+        countU = countU + 1
+        countA = countA + 1
+        
+        #print(str(row['globalIndex'])+":"+str(row[1]))
         if(isinstance( row[2], int )):
             tmpdate = datetime.datetime.fromtimestamp(row[2]/1000).strftime('%Y-%m-%d %H:%M:%S')
         else:
             tmpdate = row[2]
-        if(tmpdate >= uploadDate):
-            uploadDate = tmpdate
+
+        if(firstUDate == ""):
+            firstUDate =  tmpdate           
+        if(tmpdate >= endUDate):
+            endUDate = tmpdate
         else:
-            print("Error actual:" + uploadDate + "next one:" + tmpdate)
-            print(count)
-            print(row)
-            
-            count = 0    
-            uploadDate = tmpdate
-                    
-        if(row[7] >= mdate):
-                mdate = row[7]
+            #print("U:" + firstDate + ":" + tmpdate +":" + str(count))
+            errorULog = {"Type":['U'],"StartDate": [firstUDate], "EndDate" : [endUDate],"Count" : [countU], "Error": ['Y'], "HashID": [hashId] }
+            tmpDf = pd.DataFrame.from_dict(errorULog)
+            errorLogCollector.append(tmpDf)
+            print(errorULog)
+            firstUDate = tmpdate
+            endUDate = tmpdate
+            #print(count)
+            #print(row)        
+            countU = 0    
+        if(firstADate == ""):
+            firstADate =  row[7]                      
+        if(row[7] >= endADate):
+            endADate = row[7]            
         else:
-            print("Error actual:" + mdate + "next one:" + row[7])
-            print(row)
-            print(count)
-            count = 0
-            mdate = row[7]
+            errorALog = {"Type":['A'],"StartDate": [firstADate], "EndDate" : [endADate],"Count" : [countA], "Error": ['Y'], "HashID": [hashId] }
+            tmpDf = pd.DataFrame.from_dict(errorALog)
+            errorLogCollector.append(tmpDf)
+            print(errorALog)
+            #print(count)
+            countA = 0
+            firstADate = row[7]
+            endADate = row[7]
+
+    if(countA != 0):
+        errorALog = {"Type":['A'],"StartDate": [firstADate], "EndDate" : [endADate],"Count" : [countA], "Error": ['N'], "HashID": [hashId] }
+        print("Without error:"+str(errorALog))
+        tmpDf = pd.DataFrame.from_dict(errorALog)
+        errorLogCollector.append(tmpDf)
+        
+    if(countU != 0):
+        errorULog = {"Type":['U'],"StartDate": [firstUDate], "EndDate" : [endUDate],"Count" : [countU], "Error": ['N'], "HashID": [hashId] }
+        print("Without error:"+str(errorULog))
+        tmpDf = pd.DataFrame.from_dict(errorULog)
+        errorLogCollector.append(tmpDf)   
+    print(errorLogCollector.head(100))    
+
 
 def removeFiles():
     global userSpecificPreprocessedCausalityReports
