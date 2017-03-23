5b7d4271cff290357591e9e17d240504853e638d
Bilicki Vilmos <bilickiv@Bilickis-iMac.local>
2017-03-23 22:02:48 +0100

    debug

--- statisticsGenerators/duplicateStat.py
+++ statisticsGenerators/duplicateStat.py
@@ -84,7 +84,7 @@ def mainCycle(val):
     #LOAD DATA
     data = pd.read_csv(val, header=-1, sep=';')
     #print(data.head(1))
-    data['uploadDate'] = pd.to_datetime(df[2],unit='ms')
+    data['uploadDate'] = pd.to_datetime(data[2],unit='ms')
     data['StripedUploadDate'] = data['uploadDate'].apply(lambda A: A.date())
     data['StripedUploadDate'].apply(count_dayly_record)
     a = data[data.duplicated(subset=[3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34],keep=False)]
