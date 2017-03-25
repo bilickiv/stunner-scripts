127ef365e030defdfbd1c450c8334e34c85d3065
bilickiv <bilickiv@gmail.com>
2017-03-25 10:05:04 +0100

    small changes

--- preprocessing/datacleaning/duplicateFilter/duplicationCleaner.py
+++ preprocessing/datacleaning/duplicateFilter/duplicationCleaner.py
@@ -80,6 +80,11 @@ def removeDuplicates(data, filename):
     data['globalIndex'] = fileNumber
     data['duplicated'] = data.duplicated(subset=[5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36] , keep='first')
     tmp = data.loc[data['duplicated'] == False]
+    c = data.loc[data['duplicated'] == True]
+    print(len(c))
+    print(c.head())
+    print(len(data))
+    print(len(tmp))
     tmp['fileName'] = filename
     tmp['uploadDate'] = tmp[2].map(correctUploadDate)
     #print(tmp.tail(100))
