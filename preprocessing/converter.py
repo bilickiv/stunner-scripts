08800942a1b9743e11bdc5ca7d29c97a40e0ae46
Bilicki Vilmos <bilickiv@Bilickis-iMac.local>
2017-03-25 19:05:36 +0100

    fixed

--- preprocessing/converter.py
+++ preprocessing/converter.py
@@ -12,18 +12,23 @@ import scipy.stats as sc
 import math
 import warnings
 warnings.filterwarnings('ignore')
+def timeToTimeStamp(val):
+    try:
+        int(val)
+    except ValueError:
+        val = pd.to_datetime(val)
+        val = time.mktime(val.timetuple())
+    else:
+        return val
+    return val
+
 for fileName in glob.glob("/home/bilickiv/data/raw_dataset/duplicateFree/*.csv"):
     data = pd.read_csv(fileName, header=0, sep=';')
     print("processing:" + fileName)
-    data['0'] =  pd.to_datetime(data['0'])
-    data['0'] = data['0'].astype('int64')//10**9
-    data['1'] =  pd.to_datetime(data['1'])
-    data['1'] = data['1'].astype('int64')//10**9
-    data['4'] =  pd.to_datetime(data['4'])
-    data['4'] = data['4'].astype('int64')//10**9
-    data['9'] =  pd.to_datetime(data['9'])
-    data['9'] = data['9'].astype('int64')//10**9
-    data['uploadDate'] =  pd.to_datetime(data['uploadDate'])
-    data['uploadDate'] = data['uploadDate'].astype('int64')//10**9
+    data['0'] =  data['0'].apply(timeToTimeStamp)
+    data['1'] =  data['1'].apply(timeToTimeStamp)
+    data['4'] =  data['4'].apply(timeToTimeStamp)
+    data['9'] =  data['9'].apply(timeToTimeStamp)
+    data['uploadDate'] =  data['uploadDate'].apply(timeToTimeStamp)
     head, tail = os.path.split(fileName)
     data.to_csv("/home/bilickiv/data/raw_dataset/duplicateFree-timestamp/"+str(tail), sep='\t', encoding='utf-8')
