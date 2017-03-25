c182592653de634dc05122c7fb130ffcd4803181
Bilicki Vilmos <bilickiv@Bilickis-iMac.local>
2017-03-25 22:10:36 +0100

    converter in batch

---
+++ preprocessing/datacleaning/converter/converter.py
@@ -0,0 +1,103 @@
+import sys
+import os
+import datetime
+import time
+import glob
+import configparser
+import pandas as pd
+import numpy as np
+import argparse
+from itertools import islice
+import scipy.stats as sc
+import math
+import warnings
+warnings.filterwarnings('ignore')
+fileStep = 500
+fileStepCount = 0
+fileList = []
+indexEntries = {}
+hashIds = set()
+userSpecificPreprocessedFolder = ""
+duplicateFreetimestamp = ""
+def loadConfiguration():
+    global userSpecificPreprocessedFolder
+    global duplicateFree   
+    global duplicateFreetimestamp   
+    global userSpecificFiles
+    global fileStepCount
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("opsystem", help="runntime, 0=benti, 1=linux, 2=osx",type=int)
+    parser.add_argument("chunk", help="chunk number, 0-12",type=int)                    
+    args = parser.parse_args()
+    fileStepCount = args.chunk
+    print("Actual step:" + "----" + str(fileStepCount))
+    if(args.opsystem == 2):
+        actualEnvironment = "osx"
+    if(args.opsystem == 0):
+        actualEnvironment = "benti"
+    if(args.opsystem == 1):
+        actualEnvironment = "linux" 
+    config = configparser.ConfigParser()
+    config.read('converter.txt')
+    if(actualEnvironment == "osx"):
+        duplicateFreetimestamp = config['osx']['duplicateFree-timestamp']              
+        duplicateFree = config['osx']['duplicateFree']
+    if(actualEnvironment == "benti"):
+        duplicateFreetimestamp = config['benti']['duplicateFree-timestamp']              
+        duplicateFree = config['benti']['duplicateFree']                        
+    if(actualEnvironment == "linux"):
+        duplicateFreetimestamp = config['fict']['duplicateFree-timestamp']            
+        duplicateFree = config['fict']['duplicateFree']            
+    return;
+def timeToTimeStamp(val):
+    try:
+        x = float(val)
+        if math.isnan(x):
+            return val
+    except ValueError:
+        val = pd.to_datetime(val)
+        val = time.mktime(val.timetuple())
+    else:
+        return val
+    return val
+def loadchunks():
+    global duplicateFree
+    global fileStepCount
+    global fileStep
+    indexCounter = 0
+    fileList = []
+    for fileName in glob.glob(duplicateFree + "*.csv"):
+        fileList.append(fileName)
+    print(str(fileStep) + ":" + str(fileStepCount))
+    start = fileStepCount * fileStep
+    end = (fileStepCount + 1) * fileStep
+    iter = islice(fileList, start, end, None)
+    # loadFile(userSpecificFiles+"a2hFd3IrTHpIVHZJb1NhaU45R0xIT0h6KzloSTA1VzV4dmJmYnRVaDFhVT0.imp")
+    for a in iter:
+        mainCycle(a)
+        indexCounter = indexCounter + 1
+        print("Loaded file (" + str(indexCounter) + "):" + a)
+    return
+def mainCycle(fname):
+    #for fname in glob.glob(userSpecificPreprocessedFolder+"*.csv"):
+    global summarylogRow
+    global duplicateFreetimestamp
+    head, tail = os.path.split(fname)
+    filename = tail.split('.')[0] 
+    data = pd.read_csv(fname, header=0, sep=';')
+    print("processing:" + fname)
+    data['0'] =  data['0'].apply(timeToTimeStamp)
+    data['1'] =  data['1'].apply(timeToTimeStamp)
+    data['4'] =  data['4'].apply(timeToTimeStamp)
+    data['9'] =  data['9'].apply(timeToTimeStamp)
+    data['uploadDate'] =  data['uploadDate'].apply(timeToTimeStamp)
+    head, tail = os.path.split(fname)
+    data.to_csv(duplicateFreetimestamp+str(tail), sep='\t', encoding='utf-8')
+    return
+loadConfiguration()
+loadchunks()
+
+
+
+
---
+++ preprocessing/datacleaning/timeOverlap/timeOverlapHandler.py
@@ -0,0 +1,246 @@
+import sys
+import os
+import datetime
+import time
+import glob
+import configparser
+import pandas as pd
+import numpy as np
+import argparse
+from itertools import islice
+import scipy.stats as sc
+import math
+import warnings
+warnings.filterwarnings('ignore')
+def merge(old, new):
+    
+    return old
+def time_zone_normalization(timezone):
+    if math.isnan(timezone):
+        return 0
+    return timezone
+def to_localized_data(date):
+    print(date)
+    return date
+def estimateSpeed(data):    
+    percentage = data['percentage']
+    timestamp = data['AndroidTime']
+    speed = 0
+    #print(timestamp)
+    if(str(timestamp) != "NaT"):
+        percentageDelta = 0
+        if estimateSpeed.previousDate == None:
+            estimateSpeed.previousDate = timestamp
+            estimateSpeed.previousPercentage = percentage
+        else:
+            timeDelta = (timestamp - estimateSpeed.previousDate).seconds
+            if(timeDelta == 0):
+                timeDelta = 1/60
+            else:
+                timeDelta = timeDelta / 60
+            percentageDelta = abs(percentage - estimateSpeed.previousPercentage)
+            if(percentageDelta > 1):
+            #print("DateDelta:" + str(timeDelta) + "---"+str(timestamp) +"-" + str(estimateSpeed.previousDate))
+            #print("D:" + str(percentageDelta) +"A:" + str(percentage) + "P:" + str(estimateSpeed.previousPercentage))
+                speed = percentageDelta / timeDelta
+            estimateSpeed.previousDate = timestamp
+            estimateSpeed.previousPercentage = percentage
+    return speed
+def charging_lambda(percentage):
+    charging_trend.previous = 1000
+    charging_trend.error = 0
+    return percentage.apply(charging_trend)
+def discharging_lambda(percentage):
+    charging_trend.previous = 1000
+    charging_trend.error = 0
+    return percentage.apply(discharging_trend)
+def discharging_trend(val):
+    tmp = ""
+    if charging_trend.previous >= val:
+        tmp = True
+    else:
+        charging_trend.error  +=1
+        tmp = False
+    charging_trend.previous = val
+    return tmp
+def charging_trend(val):
+    tmp = ""
+    if val - charging_trend.previous > delta:
+        tmp = True
+    else:
+        charging_trend.error  +=1
+        tmp = False
+    charging_trend.previous = val
+    return tmp
+def percentage_break_charging_tmp(percentage):
+    return percentage.apply(percentage_break_charging)
+def percentage_break_charging(val):
+    tmp = ""
+    if percentage_break_charging.previous == None:
+        percentage_break_charging.previous = val
+        tmp = False
+    if(val > percentage_break_charging.previous):
+        if (val - percentage_break_charging.previous) > charging_delta:
+            tmp = True
+        else:
+            tmp = False
+    else:
+        if (percentage_break_charging.previous - val) > discharging_delta:
+            tmp = True
+        else:
+            tmp = False
+    percentage_break_charging.previous = val
+    return tmp
+def group_creator_by_val(val):
+    if val == group_creator_by_val.previous:
+        tmp = ""
+    else:
+        group_creator_by_val.previous = val
+        group_creator_by_val.count  +=1
+    return group_creator_by_val.count
+def date_group_creator_by_val(val):
+    val = val.date()
+    if val == date_group_creator_by_val.previous:
+        tmp = ""
+    else:
+        date_group_creator_by_val.previous = val
+        date_group_creator_by_val.count  +=1
+    return date_group_creator_by_val.count
+def selectOverloadedRows():
+    global chargingData
+    overloadedRows = pd.DataFrame()
+    for overloadedDate in overloadedDateSet:
+        tmp = chargingData[chargingData['AndroidDate'] == overloadedDate]
+        #print(len(tmp))
+        print("Before:" + str(len(chargingData)))
+        chargingData = chargingData[chargingData.AndroidDate != overloadedDate]
+        overloadedRows = pd.concat([overloadedRows, tmp], ignore_index=True)
+        print("After:"+str(len(chargingData)))
+        print(len(overloadedRows))
+    return overloadedRows
+delta = 6
+charging_delta = 20
+discharging_delta = 4
+percentage_break_charging.previous = None
+estimateSpeed.previousDate = None
+estimateSpeed.previousPercentage = None
+charging_trend.previous = 1000
+charging_trend.error = 0
+group_creator_by_val.count = 0 
+group_creator_by_val.previous = None
+date_group_creator_by_val.previous = None
+date_group_creator_by_val.count = 0
+overloadedDateSet = set()
+#pd.set_option('display.max_columns', 10)
+pd.options.display.max_columns = None
+pd.options.display.width = 120
+pd.set_option('display.max_rows', 500)
+break_after_new_order = 0
+charging_error = 0
+big_changes = 0
+total_rows = 0
+chargingData = 0
+def mainCycle(val):
+    global break_after_new_order
+    global charging_error
+    global big_changes
+    global total_rows
+    global chargingData
+    #LOAD DATA
+    data = pd.read_csv(val, header=0, sep=';')
+    #print(data.head())
+    total_rows = len(data)
+    if 'globalIndex' not in data.columns:
+        print("Not present globalIndex")
+        return
+    #print(data.head(10))
+    #CREATE DATE FROM STRING
+    data['AndroidDate'] = pd.to_datetime(data['9'])
+    #Server date to date
+    data['uploadDate'] = pd.to_datetime(data['0'])
+    #ORDER BY ANDROID DATE AND SERVERSIDE ROW
+    data = data.sort_values(by=['AndroidDate', '3'], ascending=[True,True])
+    #SELECT SUBSET:'pluggedState', 'voltage', 'temperature', 'percentage', 'health', 'chargingState','AndroidTime', 'ServerSideRow','AndroidTimezone'
+    chargingData = data[['17','18','19','20','21','22','AndroidDate', '3','36','globalIndex','uploadDate','34','4']]
+    chargingData.columns = ['pluggedState', 'voltage', 'temperature', 'percentage', 'health', 'chargingState','AndroidTime', 'ServerSideRow','AndroidTimezone','globalIndex','uploadDate','triggerCode','UploadTimestamp']
+    #print(chargingData.head(10))
+
+
+    #HANDLE NAN values (0) 
+    chargingData['AndroidTimezone'] = chargingData['AndroidTimezone'].apply(time_zone_normalization)
+    #ADD TIMEZONE TO ACTUAL ANDROID TIME
+    chargingData['localizedDate'] = chargingData['AndroidTime'] + pd.to_timedelta(chargingData.AndroidTimezone, unit='h')
+    #ADD DATE ONLY FIELD
+    chargingData['AndroidDate'] = chargingData['localizedDate'].apply(lambda A: A.date())
+    #print(chargingData[['localizedDate', 'AndroidDate', 'AndroidTimezone']].head())
+    #ANDROID TIME TO INDEX
+    chargingData.index = chargingData['localizedDate']
+    #print(chargingData.head())
+    chargingData.sort_index(inplace=True)
+
+    #gives a set with speed and Android date
+    s = chargingData.apply(estimateSpeed, axis=1)
+    s = s[s > 10]
+    #print(s.head(40))
+    if(len(s) > 0):
+        big_changes = len(s)
+        #Creates a set with date objects
+        overloadedDateSet.update(s.index.date)
+        print(overloadedDateSet)
+        #print(selectOverloadedRows(chargingData))
+        #SELECT ROWS BY Android DATE IN overloadedDateSet
+        dataToBeCorrected = selectOverloadedRows()
+        print("After return:" + str(len(chargingData)))
+        #SORT the selected rows with the help of file name and db or file row
+        dataToBeCorrected = dataToBeCorrected.sort_values(by=['globalIndex', 'ServerSideRow'], ascending=[True,True])
+        correctdData = merge(chargingData,dataToBeCorrected)
+
+        print(chargingData[['localizedDate', 'AndroidDate', 'percentage', 'ServerSideRow']])        
+        print(dataToBeCorrected[['localizedDate', 'AndroidDate', 'percentage', 'ServerSideRow']])        
+        #print(s.index.date)
+        #print(chargingData.head(100))
+        #create group by the new order and the cahrging state (each statechange increases an index which will be the group)
+        correctdData['chargingStateGroup'] = correctdData['chargingState'].apply(group_creator_by_val)
+        #create a group for each server side day
+        correctdData['serverDateStateGroup'] = correctdData['uploadDate'].apply(date_group_creator_by_val)
+        #print(dataToBeCorrected.head(100))
+        #chargingData['wrongPercentage'] = chargingData.groupby('chargingStateGroup')['percentage'].transform(charging_lambda)
+        #it defines the discharging boolean based on the chargingstategroup  the date and the trend detected with percentage change
+        correctdData['dischargingTrend'] = correctdData.groupby(['serverDateStateGroup','chargingStateGroup'])['percentage'].transform(discharging_lambda).astype('bool')
+        #it defines the charging boolean based on the chargingstategroup  the date and the trend detected with percentage change
+        correctdData['chargingTrend'] = correctdData.groupby(['serverDateStateGroup','chargingStateGroup'])['percentage'].transform(charging_lambda).astype('bool')
+        #C. csoport - törés a százalékban (lemerülés nagyobb mint 5 töltődés nagyobb mint 20)
+        correctdData['percentageBreak'] = False
+        correctdData['percentageBreak'] = correctdData.groupby(['chargingStateGroup'])['percentage'].transform(percentage_break_charging_tmp).astype('bool')
+        correctdData['chargingError']= 0
+        #if it is charging (it was detected that it is charging) and it is unplugged then this is an error
+        correctdData.ix[((correctdData['chargingTrend'] == True) &(correctdData['pluggedState'] == -1)),'chargingError']= 100
+        #print(dataToBeCorrected[dataToBeCorrected['chargingError'] == 100].head(100))
+        #print(dataToBeCorrected[['percentage','triggerCode','UploadTimestamp','chargingError','chargingStateGroup','serverDateStateGroup','chargingTrend','AndroidTimezone','percentage']].head(200))
+        #print(dataToBeCorrected[dataToBeCorrected['chargingError'] == 100])
+        #print("Charging error:" + str())
+        charging_error = len(correctdData[correctdData['chargingError'] == 100])
+        break_after_new_order = len(correctdData[correctdData.percentageBreak == True])
+        #chargingData['wrongPercentage'] = chargingData.groupby('chargingStateGroup')['wrongPercentage'].transform(lambda x: x.max())
+        #intNumber = chargingData.groupby('chargingStateGroup')
+    #chargingData[['percentage']].plot()
+#for fileName in glob.glob("/Users/bilickiv/tmpdata/duplicateFree/UGx3clFsMEsyNFRtZWh2bnozdWJ2dTRVS3hkM0pNZWVJaGNiZENjMEdpMD0.csv"):
+#for fileName in glob.glob("/Users/bilickiv/tmpdata/duplicateFree/a05TTkJkQThjNWd5SFVYd0pEV1NKUHRsZ3c4TUE2NUdlOVJaVU1XamUrdz0.csv"):
+print("START")
+logArray = []
+for fileName in glob.glob("/Users/bilickiv/tmpdata/duplicateFree/aDJtWDlLMVBWTFBwTTRpREo3VkVWdFB3THh5eWFoUFRWbW*.csv"):
+    mainCycle(fileName)
+    print("Processing file:"  + fileName)
+    head, tail = os.path.split(fileName)
+    log = {"0Rows":total_rows,"1CHERR": charging_error, "2B" : break_after_new_order,"3BC" : big_changes, "4File": tail}
+    logArray.append(log)
+    #print(log)
+    break_after_new_order = 0
+    charging_error = 0
+    big_changes = 0
+    total_rows = 0
+    output = pd.DataFrame(logArray)    
+    print(output)
+print("END")
+#output.to_csv("/Users/bilickiv/tmpdata/stat.csv")
+#print(output)
