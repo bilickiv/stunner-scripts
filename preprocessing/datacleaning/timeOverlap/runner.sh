0f3713a77513d457212af1aa88b2a3cea5599700
Bilicki Vilmos <bilickiv@Bilickis-iMac.local>
2017-03-26 11:30:36 +0200

    runner changed to timeOverlapHandler

--- preprocessing/datacleaning/timeOverlap/runner.sh
+++ preprocessing/datacleaning/timeOverlap/runner.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
-nohup python3 converter.py 1 0 > log0.txt &
-nohup python3 converter.py 1 1 > log1.txt &
-nohup python3 converter.py 1 2 > log2.txt &
-nohup python3 converter.py 1 3 > log3.txt &
-nohup python3 converter.py 1 4 > log4.txt &
-nohup python3 converter.py 1 5 > log5.txt &
-nohup python3 converter.py 1 6 > log6.txt &
-nohup python3 converter.py 1 7 > log7.txt &
-nohup python3 converter.py 1 8 > log8.txt &
-nohup python3 converter.py 1 9 > log9.txt &
-nohup python3 converter.py 1 10 > log10.txt &
-nohup python3 converter.py 1 11 > log11.txt &
-nohup python3 converter.py 1 12 > log12.txt &
-nohup python3 converter.py 1 13 > log13.txt &
-nohup python3 converter.py 1 14 > log14.txt &
-nohup python3 converter.py 1 15 > log15.txt &
-nohup python3 converter.py 1 16 > log16.txt &
-nohup python3 converter.py 1 17 > log17.txt &
-nohup python3 converter.py 1 18 > log18.txt &
-nohup python3 converter.py 1 19 > log19.txt &
-nohup python3 converter.py 1 20 > log20.txt &
-nohup python3 converter.py 1 21 > log21.txt &
+nohup python3 timeOverlapHandler.py 1 0 > log0.txt &
+nohup python3 timeOverlapHandler.py 1 1 > log1.txt &
+nohup python3 timeOverlapHandler.py 1 2 > log2.txt &
+nohup python3 timeOverlapHandler.py 1 3 > log3.txt &
+nohup python3 timeOverlapHandler.py 1 4 > log4.txt &
+nohup python3 timeOverlapHandler.py 1 5 > log5.txt &
+nohup python3 timeOverlapHandler.py 1 6 > log6.txt &
+nohup python3 timeOverlapHandler.py 1 7 > log7.txt &
+nohup python3 timeOverlapHandler.py 1 8 > log8.txt &
+nohup python3 timeOverlapHandler.py 1 9 > log9.txt &
+nohup python3 timeOverlapHandler.py 1 10 > log10.txt &
+nohup python3 timeOverlapHandler.py 1 11 > log11.txt &
+nohup python3 timeOverlapHandler.py 1 12 > log12.txt &
+nohup python3 timeOverlapHandler.py 1 13 > log13.txt &
+nohup python3 timeOverlapHandler.py 1 14 > log14.txt &
+nohup python3 timeOverlapHandler.py 1 15 > log15.txt &
+nohup python3 timeOverlapHandler.py 1 16 > log16.txt &
+nohup python3 timeOverlapHandler.py 1 17 > log17.txt &
+nohup python3 timeOverlapHandler.py 1 18 > log18.txt &
+nohup python3 timeOverlapHandler.py 1 19 > log19.txt &
+nohup python3 timeOverlapHandler.py 1 20 > log20.txt &
+nohup python3 timeOverlapHandler.py 1 21 > log21.txt &
 
 
 
